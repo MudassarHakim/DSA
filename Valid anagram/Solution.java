e9069ddcaa3b63e7117977a9eb7cb145e2491280
MudassarHakim <35130530+MudassarHakim@users.noreply.github.com>
2023-12-23 16:22:15 +0530

    Create Solution.java

---
+++ Valid anagram/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    //https://leetcode.com/problems/valid-anagram/description/
+    //
+    public boolean isAnagram(String s, String t) {
+        //String left="", right="";
+        if(s.length() != t.length()){
+           return false;
+        }
+        //boolean flag=true;
+        Map<Character,Integer> map = new HashMap<>();
+        for(int i=0 ; i < s.length(); i ++){
+            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+        }
+        //Map<Character,Integer> map1 = new HashMap<>();
+        for(int i=0 ; i < t.length(); i ++){
+           map.put(t.charAt(i),map.getOrDefault(t.charAt(i),0)-1);
+        }
+
+        for(int val: map.values()){
+            if(val<0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
